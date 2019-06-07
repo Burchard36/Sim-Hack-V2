90aa14bc92bf50a7586d428d6191ac8f54200e52
Dalton Burchard <48583030+Burchard36@users.noreply.github.com>
2019-06-07 04:36:57 -0700

    Update loginSystem.js

--- src/API/functions/loginSystem.js
+++ src/API/functions/loginSystem.js
@@ -1,9 +1,30 @@
 const readline = require('readline');
+var io = require('socket.io-client');
+var socket = io.connect('http://serverhouse.now.im:3000', {
+    reconnection: true
+});
+const mysql = require('mysql');
+
+//Connect to mysql database
+const con = mysql.createConnection({
+    host: "serverhouse.now.im",
+    port: "3306",
+    user: "admin",
+    password: "Kittens10@",
+    database: 'Game'
+})
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+function messages() {
+    console.log('\033c');
+    console.log('\x1b[36m%s\x1b[0m', "\t\t\t\t\t       Welcome to Sim Hack\n\n\t\t\t\t\t\tPlease choose to:");
+    console.log("\x1b[32m", "\t\t\t\t\t[login]", "\x1b[0m", "\tOR", "\x1b[32m", "\t[register]", "\x1b[0m");
+    console.log("\x1b[31m", "\t\t\t\t       Options to type are 'login' or 'register'", "\x1b[0m");
+}
 
 function loginSystem() {
     const collecter = function () {
@@ -37,7 +58,21 @@ function loginSystem() {
                                 console.log('\033[2J');
                                 process.stdout.write('\033c');
 
-                                let data = {Username: userName, }
+                                let data = { Username: userName, Email: userEmail, Password: userPassword, ConfirmPassword: userConfirmPassword };
+                                socket.emit('registerUser', data);
+                                socket.on('registerData', function (data) {
+                                    if (data.StatusCode === "Failed") {
+                                        console.log("\x1b[31m", data.Reason);
+                                        setTimeout(function () {
+                                            restartSystem();
+                                            return;
+                                        }, 2000);
+                                    } else if (data.statusCode === "Success") {
+                                        console.log("\x1b[31m");
+                                        console.log("\x1b[32m", "Sending requests to Sim Hack server...");
+                                    }
+                                    socket.removeAllListeners();
+                                });
                             })
                         })
                     })
@@ -49,6 +84,11 @@ function loginSystem() {
         });
     }
     collecter();
+}
+function restartSystem() {
+    messages();
+    loginSystem();
+    return;
 }
 
 module.exports = {
