b5ae55ede5546eab62a389e583b60d316f06c1a5
Dalton Burchard <48583030+Burchard36@users.noreply.github.com>
2019-06-07 04:36:34 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
-console.log('\033[2J');
-process.stdout.write('\033c');
+const login = require("../NodeJS Game/API/functions/loginSystem");
 const mysql = require('mysql');
-const login = require('../API/functions/loginSystem.js');
 
 //Connect to mysql database
 const con = mysql.createConnection({
-    host: "null",
-    port: "null",
-    user: "null",
-    password: "null",
-    database: 'null'
+    host: "serverhouse.now.im",
+    port: "3306",
+    user: "admin",
+    password: "Kittens10@",
+    database: 'Game'
 })
+function messages() {
+    console.log('\033c');
+    console.log('\x1b[36m%s\x1b[0m', "\t\t\t\t\t       Welcome to Sim Hack\n\n\t\t\t\t\t\tPlease choose to:");
+    console.log("\x1b[32m", "\t\t\t\t\t[login]", "\x1b[0m", "\tOR", "\x1b[32m", "\t[register]", "\x1b[0m");
+    console.log("\x1b[31m", "\t\t\t\t       Options to type are 'login' or 'register'", "\x1b[0m");
+}
 
-console.log('\x1b[36m%s\x1b[0m', "\t\t\t\t\t       Welcome to Sim Hack\n\n\t\t\t\t\t\tPlease choose to:");
-console.log("\x1b[32m", "\t\t\t\t\t[login]", "\x1b[0m", "\tOR", "\x1b[32m", "\t[register]", "\x1b[0m");
-console.log("\x1b[31m", "\t\t\t\t       Options to type are 'login' or 'register'", "\x1b[0m");
+function loadRegister() {
+    messages();
+    login.loginSystem();
+}
 
-login.loginSystem();
+loadRegister();
