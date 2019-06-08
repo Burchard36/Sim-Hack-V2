c243e5d8ae2062a2bd8c5a625b3079f42d5bb390
Dalton Burchard <48583030+Burchard36@users.noreply.github.com>
2019-06-08 01:08:04 -0700

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -28,6 +28,28 @@ function sanitizePassword(password) {
     var io = require('socket.io').listen(app);
 
 io.on('connection', function (socket) {
+    socket.on('loginUser', function (data) {
+        let Username = data.Username;
+        let Password = data.Password;
+        con.query("SELECT * FROM Users WHERE Username = ?", [Username], function (err, fields) {
+            if (err) throw err;
+            if (fields[0] === "" || fields[0] === undefined || fields[0] === null) {
+                var data = { StatusCode: "Failed", Reason: "Account does not exist." };
+                socket.emit('loginData', data);
+                return;
+            } else {
+                if (Password === fields[0].Password) {
+                    var data = { StatusCode: "Success", Reason: "" };
+                    socket.emit('loginData', data);
+                } else {
+                    var data = { StatusCode: "Failed", Reason: "Incorrect Username/Password." };
+                    socket.emit('loginData', data);
+                    return;
+                }
+            }
+        });
+    });
+
     socket.on('registerUser', function (data) {
         let Username = data.Username;
         let Email = data.Email;
@@ -39,21 +61,32 @@ io.on('connection', function (socket) {
             if (Username.length <= 20) {
                 if (emailIsValid(Email)) {
                     if (sanitizePassword(Password) && sanitizePassword(ConfirmPassword)) {
-                        if (Password.length > 3 && ConfirmPassword > 3) {
-                            con.query("SELECT * FROM 'Users' WHERE 'Username' = ?", [Username], function (err, fields) {
+                        if (Password.length > 3 && ConfirmPassword.length > 3) {
+                            // checks database
+                            con.query("SELECT * FROM Users WHERE Username = ?", [Username], function (err, fields) {
                                 if (err) throw err;
                                 if (fields[0] === undefined || fields[0] === null || fields[0] === "") {
-                                    con.query("SELECT * FROM 'Users' WHERE 'Email' = ?", [Email], function (err, fields) {
+                                    con.query("SELECT * FROM Users WHERE Email = ?", [Email], function (err, fields) {
                                         if (err) throw err;
                                         if (fields[0] === undefined || fields[0] === null || fields[0] === "") {
-                                            con.query("UPDATE 'Game' SET ")
+                                            var data = { Username: Username, Email: Email, Password: Password };
+                                            con.query("INSERT INTO Users SET ?", data, function (err) {
+                                                if (err) throw err;
+                                                var date = new Date();
+                                                console.log("[", "\x1b[34m", date, "\x1b[0m", "]", "\x1b[33m", " User account created, Username: ", "\x1b[32m", Username);
+                                                let data = { StatusCode: "Success", Reason: "Account succesffuly created!" };
+                                                socket.emit('registerData', data);
+                                                return;
+                                            });
                                         } else {
+                                            console.log("emits email");
                                             let data = { StatusCode: "Failed", Reason: "Email is taken!" };
                                             socket.emit('registerData', data);
                                             return;
                                         }
                                     });
                                 } else {
+                                    console.log("emits uername");
                                     let data = { StatusCode: "Failed", Reason: "Username is taken!" };
                                     socket.emit('registerData', data);
                                     return;
